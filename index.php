b22197b112b69ad0985d1e338a2ed69b2042daa5
mysteriou <elio007@hotmail.fr>
2017-03-04 19:29:50 +0100

    supression de formulaire creation project

--- index.php
+++ index.php
@@ -38,42 +38,6 @@ $place = new project();
 
 workplace
 
-</br>
-
-<form action = "<?php $_SERVER['PHP_SELF']?>" method  = "POST">
-
-
-creér un  projet
-<input type = "text" name= "project"  value = "">
-
-</br>
-</br>
-
-<input type = "submit" value = "creer un nouveau project">
-
-<?php 
-
-
-if(isset($_GET['site']) && !empty($_GET['site'])){
-
-
-echo "</br>".$_GET['site']."</br>";
-
-}
-
-
-if(isset($_POST['project']) && !empty($_POST['project'])){
-
-$place->newproject($_POST['project']);
-
-}
-
-?>
-
-</form>
-
-liste project
-
 <?php 
 
 $place->listproject();
